9f8f347cb0472df56bd607fae825354110c7b8f5
Github <Jay35710>
2020-10-07 11:57:19 -0400

    Initial commit

--- email.js
+++ email.js
@@ -28,45 +28,48 @@ let x = 0;
 axiosReq();
 
 function axiosReq() {
-    axios.get('https://dmo.pygeoapi.io/covid-19/collections/cases/items?f=json')
+    axios.get('https://demo.pygeoapi.io/covid-19/collections/cases/items?f=json')
         .then((response) => {
 
-            // console.log(response.data.features.length);
-            // for (let i = 0; i < 2; i++) {
-            //
-            //
-            //     var continent = "SELECT Continent_name FROM dtrends.Continent WHERE Country = ?;"
-            //
-            //     con.query(continent, [response.data.features[i].properties.Country_Region], function (err, continents) {
-            //         if (err) throw err;
-            //         var d = new Date(parseInt(response.data.features[i].properties.Last_Update));
-            //         var date = d.getFullYear() + '-' + ("0" + d.getMonth()).slice(-2) + '-' + d.getDate();
-            //         var layername = 'coronav_' + ("0" + d.getMonth()).slice(-2) + ("0" + d.getDate()).slice(-2) + d.getFullYear() + '_' + response.data.features[i].properties.Country_Region;
-            //         // console.log("get continents");
-            //         // console.log(continents[0].Continent_name);
-            //         // console.log("hello");
-            //         // console.log(layername);
-            //         var sql = "INSERT INTO dtrends.layers_bak20200909(Date, LayerName, LayerType, FirstLayer, SecondLayer, DisplayName, CaseNum, DeathNum, RecovNum," +
-            //             " ActiveNum, Latitude, Longitude,CityName, StateName, CountryName, ContinentName, Color_Confirmed, Color_Death, Color_Recovered) " +
-            //             "VALUES (?,?,'H_PKLayer','Corona_Virus','',?,?,?,?,?,?,?,'',?,?,?,'rgb(220,0,0) rgb(220,0,0) rgb(220,0,0)','rgb(0,0,0) rgb(0,0,0) rgb(0,0,0)','rgb(124,252,0) rgb(124,252,0) rgb(124,252,0)'); ";
-            //
-            //         con.query(sql, [date, layername, response.data.features[i].properties.Country_Region, response.data.features[i].properties.Confirmed,
-            //             response.data.features[i].properties.Deaths, response.data.features[i].properties.Recovered,
-            //             response.data.features[i].properties.Active, response.data.features[i].properties.Lat,
-            //             response.data.features[i].properties.Long_, response.data.features[i].properties.Province_State,
-            //             response.data.features[i].properties.Country_Region, continents[0].Continent_name], function (err, result) {
-            //             if (err) throw err;
-            //             console.log("1 record inserted");
-            //         });
-            //     });
-            // }
-            // console.log('while: ' + x);
-            // x = 1;
-            // console.log("Axios: " + x);
-            // console.log(1< x && x < 5);
-            // break
-            // return y;
-            console.log("Axios ");
+            console.log(response.data.features.length);
+            for (let i = 0; i < 10; i++) {
+
+                var continent = "SELECT Continent_name FROM dtrends.Continent WHERE Country = ?;"
+
+                con.query(continent, [response.data.features[i].properties.Country_Region], function (err, continents) {
+                    if (err) throw err;
+                    if (response.data.features[i].properties.Province_State == null) {
+                        Country_Region_Province_State = response.data.features[i].properties.Country_Region.replace(/ /g, "_")
+                        // console.log("1")
+                    } else {
+                        var Country_Region_Province_State_Combine = response.data.features[i].properties.Country_Region + "_" + response.data.features[i].properties.Province_State;
+                        Country_Region_Province_State = Country_Region_Province_State_Combine.replace(/ /g, "_")
+                        // console.log("2")
+                    }
+                    if (response.data.features[i].properties.Province_State == null) {
+                        var Province_State = null;
+                    } else {
+                        var Province_State = response.data.features[i].properties.Province_State.replace(/ /g, "_");
+                    }
+
+                    var d = new Date(parseInt(response.data.features[i].properties.Last_Update));
+                    var date = d.getFullYear() + '-' + ("0" + d.getMonth()).slice(-2) + '-' + ("0" + d.getDate()).slice(-2);
+                    var layername = 'coronav_' + ("0" + d.getMonth()).slice(-2) + ("0" + d.getDate()).slice(-2) + d.getFullYear() + '_' + Country_Region_Province_State;
+                    var sql = "INSERT INTO dtrends.layers_test(Date, LayerName, LayerType, FirstLayer, SecondLayer, DisplayName, CaseNum, DeathNum, RecovNum," +
+                        " ActiveNum, Latitude, Longitude,CityName, StateName, CountryName, ContinentName, Color_Confirmed, Color_Death, Color_Recovered) " +
+                        "VALUES (?,?,'H_PKLayer','Corona_Virus','',?,?,?,?,?,?,?,'',?,?,?,'rgb(220,0,0) rgb(220,0,0) rgb(220,0,0)','rgb(0,0,0) rgb(0,0,0) rgb(0,0,0)','rgb(124,252,0) rgb(124,252,0) rgb(124,252,0)'); ";
+
+                    con.query(sql, [date, layername, response.data.features[i].properties.Country_Region.replace(/ /g, "_"), response.data.features[i].properties.Confirmed,
+                        response.data.features[i].properties.Deaths, response.data.features[i].properties.Recovered,
+                        response.data.features[i].properties.Active, response.data.features[i].properties.Lat,
+                        response.data.features[i].properties.Long_, Province_State,
+                        response.data.features[i].properties.Country_Region.replace(/ /g, "_"), continents[0].Continent_name.replace(/ /g, "_")], function (err, result) {
+                        if (err) throw err;
+                        console.log("1 record inserted");
+                    });
+                });
+            }
+
         })
         .catch(error => {
 
@@ -78,8 +81,9 @@ function axiosReq() {
         });
 
 }
-function Timeout() {
 
+
+function Timeout() {
     setTimeout(function() {
         if (x < 10) {
             axiosReq();
---
+++ testpaper.js
@@ -0,0 +1,4 @@
+// const name = 'Hi my name is Flavio'
+// const nameCleaned = name.replace(/ /g,"_")
+//
+// console.log(nameCleaned);
