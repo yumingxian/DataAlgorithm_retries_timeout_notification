945f661cb7481418fb6bef5d76aa011e7a4dbfb3
charliecaiftaa <charlie.cai@g.feitianacademy.org>
2020-10-15 17:15:41 -0400

    4

--- email.js
+++ email.js
@@ -42,20 +42,22 @@ axiosReq();
 let diflimit = 10;
 
 function axiosReq() {
-    //sql connect checking
-    if(con.state === 'disconnected'){
-        transport.sendMail(sqlconnection, (error, info) => {
-            if (error) {
-                console.log(error);
-            }
-            console.log(`Message sent: ${info.response}`);
-        });
-    }
 
+    con.connect(function (err) {
+        if (err) {
+            transport.sendMail(sqlconnection, (error, info) => {
+                if (error) {
+                    console.log(error);
+                }
+                console.log(`Message sent: ${info.response}`);
+            });
+        }
+        console.log("MySql Connected")
+
+    });
     axios.get('https://demo.pygeoapi.io/covid-19/collections/cases/items?f=json&limit=10000')
         .then((response) => {
-
-            // console.log(response.data.features.length);
+            //console.log(response.data.features.length);
             var d = new Date();
             var y = new Date(d);
             y.setDate(y.getDate() - 1)
@@ -64,35 +66,36 @@ function axiosReq() {
             con.query(countcheck, [yesterday], function (err, count) {
                 var pointNum = parseFloat(count[0].rowscount);
                 var dif = response.data.features.length - pointNum;// minus yesterdays data length, query
-                if (Math.abs(dif) > diflimit){
-                    console.log("Differences"+":"+ dif);
+                if (Math.abs(dif) > diflimit) {
+                    console.log("Differences" + ":" + dif);
                     transport.sendMail(Datalength, (error, info) => {
                         if (error) {
                             console.log(error);
                         }
                         console.log(`Message sent: ${info.response}`);
                     });
-                }else{
+                } else {
                     for (let i = 0; i < response.data.features.length; i++) {
-                        //
+
+
                         var continent = "SELECT Continent_name FROM dtrends.Continent WHERE Country LIKE ?;"
                         var stringx = [response.data.features[i].properties.Country_Region].toString();
-                        if(stringx.includes('(')){
-                            var x = stringx.substr(0,stringx.indexOf(' '));
-                        }else if(stringx.includes(',')){
-                            var x = stringx.substr(0,stringx.indexOf(','));
+                        if (stringx.includes('(')) {
+                            var x = stringx.substr(0, stringx.indexOf(' '));
+                        } else if (stringx.includes(',')) {
+                            var x = stringx.substr(0, stringx.indexOf(','));
                             // console.log(x)
 
-                        }else if(stringx.includes('*')){
-                            var x = stringx.substr(0,stringx.indexOf('*'));
+                        } else if (stringx.includes('*')) {
+                            var x = stringx.substr(0, stringx.indexOf('*'));
                             // console.log(x)
-                        }else{
+                        } else {
                             var x = stringx;
                         }
                         // console.log(x)
 
 
-                        con.query(continent, "%"+ x + "%" , function (err, continents) {
+                        con.query(continent, "%" + x + "%", function (err, continents) {
                             //     // console.log(response.data.features[i].properties.Country_Region);
                             //     console.log(i, ": ", continents[0].Continent_name,response.data.features[i].properties.Country_Region);
                             if (response.data.features[i].properties.Province_State == null) {
@@ -108,29 +111,37 @@ function axiosReq() {
                             }
 
                             var d = new Date(parseInt(response.data.features[i].properties.Last_Update));
-                            var date = d.getFullYear() + '-' + ("0" + (d.getMonth()+1)).slice(-2) + '-' + ("0" + d.getDate()).slice(-2);
-                            var layername = 'coronav_' + ("0" + (d.getMonth()+1)).slice(-2) + ("0" + d.getDate()).slice(-2) + d.getFullYear() + '_' + Country_Region_Province_State;
+                            var date = d.getFullYear() + '-' + ("0" + (d.getMonth() + 1)).slice(-2) + '-' + ("0" + d.getDate()).slice(-2);
+                            var layername = 'coronav_' + ("0" + (d.getMonth() + 1)).slice(-2) + ("0" + d.getDate()).slice(-2) + d.getFullYear() + '_' + Country_Region_Province_State;
                             var sql = "INSERT INTO dtrends.layers_test(Date, LayerName, LayerType, FirstLayer, SecondLayer, DisplayName, CaseNum, DeathNum, RecovNum," +
                                 " ActiveNum, Latitude, Longitude,CityName, StateName, CountryName, ContinentName, Color_Confirmed, Color_Death, Color_Recovered) " +
                                 "VALUES (?,?,'H_PKLayer','Corona_Virus','',?,?,?,?,?,?,?,'',?,?,?,'rgb(220,0,0) rgb(220,0,0) rgb(220,0,0)','rgb(0,0,0) rgb(0,0,0) rgb(0,0,0)','rgb(124,252,0) rgb(124,252,0) rgb(124,252,0)'); ";
 
+                            // var deleting = "DELETE FROM dtrends.layers_test WHERE Date = ?;"
 
                             con.query(sql, [date, layername, response.data.features[i].properties.Country_Region.replace(/ /g, "_"), response.data.features[i].properties.Confirmed,
                                 response.data.features[i].properties.Deaths, response.data.features[i].properties.Recovered,
                                 response.data.features[i].properties.Active, response.data.features[i].properties.Lat,
                                 response.data.features[i].properties.Long_, Province_State,
                                 response.data.features[i].properties.Country_Region.replace(/ /g, "_"), continents[0].Continent_name.replace(/ /g, "_")], function (err, result) {
-                                if (err){
-                                    console.log(err);}
-                                // console.log("1 record inserted");
+                                // if (err = "ER_DUP_ENTRY") {
+                                //     con.query(deleting, [date], function (err, result) {
+                                //         if (err) {
+                                //             console.log(err);
+                                //         }
+                                //         console.log("Duplicate Layers deleted")
+                                //
+                                //     });
 
-
+                                // }
+                                    console.log(i + "record inserted");
+                                // }
 
 
                             });
+
                         });
                     }
-                    console.log("Success");
                 }
 
 
@@ -143,11 +154,12 @@ function axiosReq() {
 
         });
 
+
 }
 
 
 function Timeout() {
-    setTimeout(function() {
+    setTimeout(function () {
         if (x < 10) {
             axiosReq();
             // setTimeout(axiosReq(), 10000);
